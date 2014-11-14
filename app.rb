88958740b1a2bb41088e25cf8760bb60933eab3b
Emmanuelle Stermann <ricomanue@gmail.com>
2014-11-14 23:01:38 +0100

    maj

--- app.rb
+++ app.rb
@@ -3,30 +3,42 @@ require 'pry'
 require 'data_mapper'
 require 'rubygems'
 
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/blog.db")
+
 class Log
   include DataMapper::Resource
   property :id, Serial
-  property :title, String
-  property :body, Text
+  property :nom, String
+  property :email, String
+  property :message, Text
   property :created_at, DateTime
 end
 
-@@logs =["testy","tosty","tasty"]
+DataMapper.finalize
+Log.auto_upgrade!
 
 get '/about' do
   erb :about
 end
 
 get '/' do
-	# DÉFINIR LA VARIABLE @LOGS
+	@logs = Log.all
 	erb :vueindex
 end
 
-post '/' do
-	@@logs << params[:log]
-	erb :vueindex
+get '/delete/:id' do
+ Log.get(params[:id]).destroy 
+ redirect '/'
+  end
+
+post '/update/:id' do
+@log = Log.get(params[:id])
+@log.update(:auteur => params[:auteur],:contenu => params[:log])
+redirect '/'
 end
 
-#post'/vueindex' do
-  #binding.pry
-#end
+
+post'/vueindex' do
+  Log.create(email: params[:email], nom: params[:nom] , message: params[:message] )
+  redirect '/'
+end
